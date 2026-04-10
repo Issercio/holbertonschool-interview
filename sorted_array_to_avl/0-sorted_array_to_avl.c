4e4fe6540f1f5cdf10f3f774e264573b5cecade1
Issercio <didi_1374@outlook.fr>
2026-04-10 10:00:49 +0200

    add the project

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -10,20 +10,20 @@
  */
 avl_t *create_avl(int *array, int start, int end, avl_t *parent)
 {
-    avl_t *root;
-    int mid;
+	avl_t *root;
+	int mid;
 
-    if (start > end)
-        return (NULL);
-    mid = (start + end) / 2;
-    root = malloc(sizeof(avl_t));
-    if (!root)
-        return (NULL);
-    root->n = array[mid];
-    root->parent = parent;
-    root->left = create_avl(array, start, mid - 1, root);
-    root->right = create_avl(array, mid + 1, end, root);
-    return (root);
+	if (start > end)
+		return (NULL);
+	mid = (start + end) / 2;
+	root = malloc(sizeof(avl_t));
+	if (!root)
+		return (NULL);
+	root->n = array[mid];
+	root->parent = parent;
+	root->left = create_avl(array, start, mid - 1, root);
+	root->right = create_avl(array, mid + 1, end, root);
+	return (root);
 }
 
 /**
@@ -34,7 +34,7 @@ avl_t *create_avl(int *array, int start, int end, avl_t *parent)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return (NULL);
-    return (create_avl(array, 0, (int)size - 1, NULL));
+	if (!array || size == 0)
+		return (NULL);
+	return (create_avl(array, 0, (int)size - 1, NULL));
 }

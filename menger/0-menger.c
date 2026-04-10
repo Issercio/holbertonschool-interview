e7acf9ab09b9e58f5fc7715e17991e454a54bda3
Issercio <didi_1374@outlook.fr>
2026-04-10 09:45:38 +0200

    add the project

---
+++ menger/0-menger.c
@@ -0,0 +1,36 @@
+#include "menger.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * menger - Draws a 2D Menger sponge of a given level
+ * @level: The level of the Menger sponge
+ */
+void menger(int level)
+{
+    int size, row, col, x, y, draw;
+    if (level < 0)
+        return;
+    size = pow(3, level);
+    for (row = 0; row < size; row++)
+    {
+        for (col = 0; col < size; col++)
+        {
+            x = row;
+            y = col;
+            draw = 1;
+            while (x > 0 || y > 0)
+            {
+                if (x % 3 == 1 && y % 3 == 1)
+                {
+                    draw = 0;
+                    break;
+                }
+                x /= 3;
+                y /= 3;
+            }
+            putchar(draw ? '#' : ' ');
+        }
+        putchar('\n');
+    }
+}

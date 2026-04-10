48e1a8580ad49676e8b09b54017d4861d7d42127
Issercio <didi_1374@outlook.fr>
2026-04-10 09:48:34 +0200

    add the project

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,3 +1,4 @@
+
 #include "menger.h"
 #include <stdio.h>
 #include <math.h>
@@ -8,29 +9,30 @@
  */
 void menger(int level)
 {
-    int size, row, col, x, y, draw;
-    if (level < 0)
-        return;
-    size = pow(3, level);
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            x = row;
-            y = col;
-            draw = 1;
-            while (x > 0 || y > 0)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    draw = 0;
-                    break;
-                }
-                x /= 3;
-                y /= 3;
-            }
-            putchar(draw ? '#' : ' ');
-        }
-        putchar('\n');
-    }
+	int size, row, col, x, y, draw;
+
+	if (level < 0)
+		return;
+	size = pow(3, level);
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			x = row;
+			y = col;
+			draw = 1;
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					draw = 0;
+					break;
+				}
+				x /= 3;
+				y /= 3;
+			}
+			putchar(draw ? '#' : ' ');
+		}
+		putchar('\n');
+	}
 }

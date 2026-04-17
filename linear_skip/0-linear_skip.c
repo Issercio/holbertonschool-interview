6e61ae9156e741bb525c8a331d689c8833396b16
Issercio <didi_1374@outlook.fr>
2026-04-17 09:18:37 +0200

    add the project

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -9,39 +9,40 @@
  */
 skiplist_t *linear_skip(skiplist_t *head, int value)
 {
-    skiplist_t *prev, *curr;
+	skiplist_t *prev, *curr;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    prev = head;
-    curr = head->express;
+	prev = head;
+	curr = head->express;
 
-    while (curr)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-        if (curr->n >= value)
-            break;
-        prev = curr;
-        curr = curr->express;
-    }
+	while (curr)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+		if (curr->n >= value)
+			break;
+		prev = curr;
+		curr = curr->express;
+	}
 
-    if (!curr)
-    {
-        curr = prev;
-        while (curr->next)
-            curr = curr->next;
-    }
+	if (!curr)
+	{
+		curr = prev;
+		while (curr->next)
+			curr = curr->next;
+	}
 
-    printf("Value found between indexes [%lu] and [%lu]\n", prev->index, curr->index);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		prev->index, curr->index);
 
-    while (prev && prev->index <= curr->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-        if (prev->n == value)
-            return (prev);
-        prev = prev->next;
-    }
+	while (prev && prev->index <= curr->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }

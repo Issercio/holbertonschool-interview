05507d98c1b68369c007de8dd75447aa5370deca
Issercio <didi_1374@outlook.fr>
2026-07-24 09:20:16 +0200

    add the project

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * add_node_end - adds a node at the end of a double circular linked list
+ * @list: address of the head of the list
+ * @str: string to duplicate into the new node
+ *
+ * Return: address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+	List *tail;
+
+	if (list == NULL || str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(List));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	if (*list == NULL)
+	{
+		new->next = new;
+		new->prev = new;
+		*list = new;
+		return (new);
+	}
+
+	tail = (*list)->prev;
+
+	new->next = *list;
+	new->prev = tail;
+	tail->next = new;
+	(*list)->prev = new;
+
+	return (new);
+}
+
+/**
+ * add_node_begin - adds a node at the beginning of a double circular list
+ * @list: address of the head of the list
+ * @str: string to duplicate into the new node
+ *
+ * Return: address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	new = add_node_end(list, str);
+	if (new == NULL)
+		return (NULL);
+
+	*list = new;
+
+	return (new);
+}

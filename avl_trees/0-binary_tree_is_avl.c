137a00bc24534d32b7871be451129826479631a5
Issercio <didi_1374@outlook.fr>
2026-08-14 09:22:00 +0200

    add the project

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -52,23 +52,20 @@ static int binary_tree_is_bst(const binary_tree_t *tree, long min, long max)
 }
 
 /**
- * binary_tree_is_avl - Checks if a binary tree is a valid AVL tree.
+ * binary_tree_check_avl - Checks if a tree is balanced.
  *
- * @tree: Pointer to the root node of the tree.
+ * @tree: Pointer to the root node.
  *
- * Return: 1 if tree is a valid AVL tree, 0 otherwise.
+ * Return: 1 if the tree is AVL balanced, 0 otherwise.
  */
-int binary_tree_is_avl(const binary_tree_t *tree)
+static int binary_tree_check_avl(const binary_tree_t *tree)
 {
 	int left_height;
 	int right_height;
 	int difference;
 
 	if (tree == NULL)
-		return (0);
-
-	if (!binary_tree_is_bst(tree, -2147483649L, 2147483648L))
-		return (0);
+		return (1);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
@@ -78,11 +75,29 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	if (difference < -1 || difference > 1)
 		return (0);
 
-	if (!binary_tree_is_avl(tree->left))
+	if (!binary_tree_check_avl(tree->left))
 		return (0);
 
-	if (!binary_tree_is_avl(tree->right))
+	if (!binary_tree_check_avl(tree->right))
 		return (0);
 
 	return (1);
+}
+
+/**
+ * binary_tree_is_avl - Checks if a binary tree is a valid AVL tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: 1 if tree is a valid AVL tree, 0 otherwise.
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (!binary_tree_is_bst(tree, -2147483649L, 2147483648L))
+		return (0);
+
+	return (binary_tree_check_avl(tree));
 }
